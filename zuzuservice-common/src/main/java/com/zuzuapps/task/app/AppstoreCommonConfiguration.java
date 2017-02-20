a9a85623e638695ff8e72dfe7d8d228679335095
Tran Anh Tuan <tuan.tran@fujixerox.co.jp>
2017-02-20 10:11:16 +0900

    Change structure

--- zuzuservice-common/src/main/java/com/zuzuapps/task/app/AppstoreCommonConfiguration.java
+++ zuzuservice-common/src/main/java/com/zuzuapps/task/app/AppstoreCommonConfiguration.java
@@ -3,7 +3,6 @@ package com.zuzuapps.task.app;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.solr.server.support.MulticoreSolrClientFactory;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -15,7 +14,7 @@ import java.util.List;
  * @author tuanta17
  */
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
-@EnableElasticsearchRepositories(basePackages = "com.zuzuapps.task.app.elasticsearch.repositories")
+//@EnableElasticsearchRepositories(basePackages = "com.zuzuapps.task.app.elasticsearch.repositories")
 //@EnableSolrRepositories(basePackages={"com.zuzuapps.task.app.solr.repositories"}, multicoreSupport=true)
 public class GooglePlayCommonConfiguration {
 
