e365c4ec52b8c1e5a00a96f6d19c126c6bb063d3
Tran Anh Tuan <tk1cntt@gmail.com>
2017-03-01 22:43:39 +0900

    Pageable

--- zuzuservice-common/src/main/java/com/zuzuapps/task/app/solr/repositories/AppIndexSolrRepository.java
+++ zuzuservice-common/src/main/java/com/zuzuapps/task/app/solr/repositories/AppIndexSolrRepository.java
@@ -1,6 +1,8 @@
 package com.zuzuapps.task.app.solr.repositories;
 
 import com.zuzuapps.task.app.solr.models.AppIndexSolr;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 
@@ -10,5 +12,5 @@ import java.util.List;
  * @author tuanta17
  */
 public interface AppIndexSolrRepository extends SolrCrudRepository<AppIndexSolr, String> {
-    List<AppIndexSolr> findByCountryCodeAndCategoryAndCollection(@Param("countryCode") String countryCode, @Param("category") String category, @Param("collection") String collection);
+    Page<AppIndexSolr> findByCountryCodeAndCategoryAndCollection(@Param("countryCode") String countryCode, @Param("category") String category, @Param("collection") String collection, Pageable pageable);
 }
