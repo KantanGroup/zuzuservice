c5e7724fe636bdc9c342bead09947d993a903f42
Tran Anh Tuan <tk1cntt@gmail.com>
2017-05-14 22:47:39 +0700

    Version 1.2.2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mvn clean install -DskipTests
-cp zuzuservice-app-summary-scheduled/target/google-play-app-summary-scheduled-1.2.1.jar ~/appstore/zuzuservice.jar
+cp zuzuservice-app-summary-scheduled/target/google-play-app-summary-scheduled-1.2.2.jar ~/appstore/zuzuservice.jar
 cp countries.json ~/appstore/
